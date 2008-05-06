e6d63dbad2101cdbce4a483591b1448ab529d5aa
mikeal <mikeal.rogers@gmail.com>
2008-05-06 20:10:34 +0000

    Initial commit.
    git-svn-id: http://mozrunner.googlecode.com/svn/trunk@2 ad6f2e34-954c-0410-b0c4-97802e43e83d

---
+++ mozrunner/install.py
@@ -0,0 +1,101 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Mozilla Corporation Code.
+#
+# The Initial Developer of the Original Code is
+# Mikeal Rogers.
+# Portions created by the Initial Developer are Copyright (C) 2008
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#  Mikeal Rogers <mikeal.rogers@gmail.com>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+import sys, os
+import tempfile
+import subprocess
+import commands
+import shutil
+
+import simplejson
+
+import mozrunner
+
+def set_preferences(profile, prefs, enable_default_prefs=True):
+    prefs_file = os.path.join(profile, 'prefs.js')
+    f = open(prefs_file, 'w+')
+    f.write('\n#MozRunner Prefs Start\n')
+    
+    if enable_default_prefs and hasattr(mozrunner, 'settings'):
+        default_prefs = mozrunner.settings.get('MOZILLA_DEFAULT_PREFS')
+        pref_lines = ['user_pref(%s, %s);' % 
+                      (simplejson.dumps(k), simplejson.dumps(v) ) for k, v in default_prefs.items()]
+        f.write('#MozRunner Default Prefs\n')
+        for line in pref_lines:
+            f.write(line+'\n')
+    
+    pref_lines = ['user_pref(%s, %s)' % 
+                  (simplejson.dumps(k), simplejson.dumps(v) ) for k, v in prefs.items()]
+    f.write('#MozRunner Preferences\n')
+    for line in pref_lines:
+        f.write(line+'\n')
+
+    f.write('#MozRunner Prefs End\n')
+    f.flush() ; f.close()
+
+def clean_prefs_file(prefs_file):
+    lines = open(prefs_file, 'r').read().splitlines()
+    s = lines.index('#MozRunner Prefs Star') ; e = lines.index('#MozRunner Prefs End')
+    cleaned_prefs = '\n'.join(lines[:s] + lines[e:])
+    f = open(prefs_file, 'w') ; f.write(cleaned_prefs) ; f.flush() ; f.close()
+
+def create_tmp_profile(settings):
+    default_profile = settings['MOZILLA_DEFAULT_PROFILE']
+    tmp_profile = tempfile.mkdtemp(suffix='.mozrunner')
+    
+    if sys.platform == 'linux2':
+        print commands.getoutput('chown -R %s:%s %s' % (os.getlogin(), os.getlogin(), tmp_profile))
+                                 
+    if os.path.exists(tmp_profile) is True:
+        shutil.rmtree(tmp_profile)
+
+    shutil.copytree(default_profile, tmp_profile)
+    settings['MOZILLA_PROFILE'] = tmp_profile
+    
+# ./firefox-bin -no-remote -profile "/Users/mikeal/Library/Application Support/windmill/firefox.profile" -install-global-extension /Users/mikeal/Desktop/jssh-firefox-3.x.xpi    
+    
+def install_plugins(settings):
+    binary = settings['MOZILLA_BINARY']
+    profile = settings['MOZILLA_PROFILE']
+    
+    for plugin_path in settings['MOZILLA_PLUGINS']:
+        command = [binary, '-no-remote', '-profile', profile, '-install-global-extension', plugin_path]
+        return_code = subprocess.call(command)
+        assert not return_code
+    
+    
+
---
+++ mozrunner/runner.py
@@ -0,0 +1,169 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Mozilla Corporation Code.
+#
+# The Initial Developer of the Original Code is
+# Mikeal Rogers.
+# Portions created by the Initial Developer are Copyright (C) 2008
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#  Mikeal Rogers <mikeal.rogers@gmail.com>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+import sys, os
+import commands
+import killableprocess
+import logging
+import signal
+import exceptions
+
+from time import sleep
+import subprocess
+
+logger = logging.getLogger(__name__)
+
+def run_command(cmd):
+    """Run the given command in killable process."""
+    kwargs = {'stdout':sys.stdout ,'stderr':sys.stderr, 'stdin':sys.stdin}
+    
+    if sys.platform != "win32":
+        return killableprocess.Popen(cmd, preexec_fn=lambda : os.setpgid(0, 0), **kwargs)
+    else:
+        return killableprocess.Popen(cmd, **kwargs)
+
+def get_pids(name, minimun_pid=0):
+    if os.name == 'nt' or sys.platform == 'cygwin':
+        import win32api, win32pdhutil, win32con
+        #win32pdhutil.ShowAllProcesses()  #uncomment for testing
+        pids = win32pdhutil.FindPerformanceAttributesByName(name)
+    
+    else:
+        get_pids_cmd = ['ps', 'ax']
+        h = killableprocess.runCommand(get_pids_cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        h.wait()
+        data = h.stdout.readlines()
+        pids = [int(line.split()[0]) for line in data if line.find(name) is not -1]
+        
+    matching_pids = [m for m in pids if m > minimun_pid]
+    return matching_pids
+
+def kill_process_by_name(name):
+    """Find and kill all processes containing a certain name"""
+    
+    pids = get_pids(name)
+    
+    if os.name == 'nt' or sys.platform == 'cygwin':
+        for p in pids:
+            handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, 0, p) #get process handle
+            win32api.TerminateProcess(handle,0) #kill by handle
+            win32api.CloseHandle(handle) #close api
+
+    else:
+        for pid in pids:
+            os.kill(pid, signal.SIGTERM)
+            sleep(.5)
+            if len(get_pids(name)) is not 0:
+                os.kill(pid, signal.SIGKILL)
+                sleep(.5)
+                if len(get_pids(name)) is not 0:
+                    logger.error('Could not kill process')        
+
+class Mozilla(object):
+    
+    CMD_ARGS = []
+    
+    def __init__(self, binary, profile, cmd_args=[], aggressively_kill=['crashreporter']):
+        self.set_profile(profile)
+        self.set_binary(binary)
+        self.set_command(cmd_args + self.CMD_ARGS)
+        self.aggressively_kill = aggressively_kill
+        
+    def set_command(self, cmd_args):
+        self.command = [self.binary]
+        self.command += cmd_args
+        self.command += ['-profile', self.profile]
+    
+    def set_binary(self, binary):
+        self.binary = binary
+            
+    def set_profile(self, profile):
+        """Set the profile path and do any cleanup or platform specific hacks"""
+        if sys.platform == 'linux2':
+            print commands.getoutput('chown -R %s:%s %s' % (os.getlogin(), os.getlogin(), profile))
+            
+        self.profile = profile
+    
+    def start(self):
+        self.process_handler = run_command(self.command)
+
+    def wait(self):
+        self.process_handler.wait()
+        
+        if sys.platform != 'win32':
+            for pid in get_pids(self.name, self.process_handler.pid):    
+                self.process_handler.pid = pid
+                self.process_handler.wait()
+         
+    def kill(self, kill_signal=signal.SIGTERM):
+        """Kill the browser"""
+        if sys.platform == 'darwin':
+            self.process_handler.kill()
+            for pid in get_pids(self.name, self.process_handler.pid):    
+                self.process_handler.pid = pid
+                self.process_handler.kill()
+        else:
+            try:
+                self.process_handle.kill(group=True)
+            except:
+                logger.error('Cannot kill process')
+                
+        for name in self.aggressively_kill:
+            kill_process_by_name(name)
+    
+    def stop(self):
+        self.kill()
+        
+        
+class Firefox(Mozilla):
+    """Firefox mozilla runner class."""
+    name = 'firefox'
+    
+    def set_binary(self, binary):
+        """Set the binary path and do any cleanup or platform specific hacks"""
+        if sys.platform == 'linux2':    
+            # This block is to fix Ubuntu's stupid shell script that breaks a bunch of options
+            new_bin = open(binary, 'r').read()
+            new_bin = new_bin.replace('file://', '')
+            new_bin = new_bin.replace('MOZILLA_BIN="${progbase}-bin"', 'MOZILLA_BIN="firefox-bin"')
+            new_bin_path = self.profile+'/'+'mozrunner-firefox'
+            f = open(new_bin_path, 'w')
+            f.write(new_bin); f.flush(); f.close()
+            subprocess.call(['chmod', '755', new_bin_path])
+            self.binary = new_bin_path
+        else:
+            self.binary = binary
